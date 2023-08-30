977cf35a0c63d94c86bdaf65bf61e9663497ba9f
Fausto Sánchez Hoya <faustosanchezhoya@gmail.com>
2023-08-30 21:01:31 +0200

    Mejorada detección de muerte y añadida penalización

--- main.py
+++ main.py
@@ -30,10 +30,9 @@ class DinoEnv(gym.Env):
     def __init__(self):
         super(DinoEnv, self).__init__()
         self.driver = webdriver.Chrome()
-        self.action_space = spaces.Discrete(2)  # Salto y agacharse
+        self.action_space = spaces.Discrete(3)  # Salto, agacharse o no hacer nada
         self.observation_space = spaces.Box(low=0, high=1, shape=(3, 80, 80), dtype=np.float32)
-        # self.TIME_THRESHOLD = 0.5
-        self.SLEEP_TIME = 0.1
+        self.DIED_CONSTANT=5000
         self.counter = 0
         self.points = 0
 
@@ -52,18 +51,15 @@ class DinoEnv(gym.Env):
         y_start, y_end, x_start, x_end = 200, 425, 150, 1000 #Coordenadas de detección de obstáculos
         y_start1, y_end1, x_start1, x_end1 = 150, 250, 0, 100 #Coordenadas de detección de salto
         y_start2, y_end2, x_start2, x_end2 = 350, 425, 0, 100 #Coordenadas de detección de dinosaurio en tierra
-        y_start3, y_end3, x_start3, x_end3 = 300, 350, 620, 700 #Coordenadas de detección de fin de partida
 
         # Recortar la región de interés
         cropped_image = image[y_start:y_end, x_start:x_end]
         cropped_image1 = image[y_start1:y_end1, x_start1:x_end1]
         cropped_image2 = image[y_start2:y_end2, x_start2:x_end2]
-        cropped_image3 = image[y_start3:y_end3, x_start3:x_end3]
 
         # cv2.rectangle(image, (x_start, y_start), (x_end, y_end), (0, 0, 255), 2)
         # cv2.rectangle(image, (x_start1, y_start1), (x_end1, y_end1), (0, 0, 255), 2)
         # cv2.rectangle(image, (x_start2, y_start2), (x_end2, y_end2), (0, 0, 255), 2)
-        # cv2.rectangle(image, (x_start3, y_start3), (x_end3, y_end3), (0, 0, 255), 2)
 
         # cv2.imshow('Region to Crop', image)
         # cv2.waitKey(0)
@@ -73,69 +69,56 @@ class DinoEnv(gym.Env):
         resized_image = cv2.resize(cropped_image, (80, 80))
         resized_image1 = cv2.resize(cropped_image1, (80, 80))
         resized_image2 = cv2.resize(cropped_image2, (80, 80))
-        resized_image3 = cv2.resize(cropped_image3, (80, 80))
 
         # Convertir a escala de grises
         grayscale_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
         grayscale_image1 = cv2.cvtColor(resized_image1, cv2.COLOR_BGR2GRAY)
         grayscale_image2 = cv2.cvtColor(resized_image2, cv2.COLOR_BGR2GRAY)
-        grayscale_image3 = cv2.cvtColor(resized_image3, cv2.COLOR_BGR2GRAY)
 
         # Normalizar los valores de los píxeles al rango [0, 1]
         normalized_image = grayscale_image / 255.0
         normalized_image1 = grayscale_image1 / 255.0
         normalized_image2 = grayscale_image2 / 255.0
-        normalized_image3 = grayscale_image3 / 255.0
 
         # Convertir la imagen en un tensor
         image_tensor = torch.tensor(normalized_image, dtype=torch.float32) #imagen de detección de obstáculos
         image_tensor1 = torch.tensor(normalized_image1, dtype=torch.float32) #imagen de detección de salto
         image_tensor2 = torch.tensor(normalized_image2, dtype=torch.float32) #imagen de detección de dinosaurio en tierra
-        image_tensor3 = torch.tensor(normalized_image3, dtype=torch.float32) #imagen de detección de fin de partida
 
-        maybeEndGame = torch.any(image_tensor3 < 0.95) #Puede detectar o no fin de partida. Hay muchas veces que dará falsos positivos por lo que hay que asegurar fin de partida comprobando que durante varios segundos esté a true
-
-
-        return image_tensor, image_tensor1, image_tensor2, maybeEndGame
+        return image_tensor, image_tensor1, image_tensor2
 
     def get_game_state(self):
         screenshot = self.driver.get_screenshot_as_png()
         image = cv2.imdecode(np.frombuffer(screenshot, np.uint8), cv2.IMREAD_COLOR)
-        image_tensor, image_tensor1, image_tensor2, maybeEndGame = self.preprocess_image(image)
+        image_tensor, image_tensor1, image_tensor2 = self.preprocess_image(image)
 
         combined_tensor = np.stack([image_tensor, image_tensor1, image_tensor2], axis=0)
-        return combined_tensor, maybeEndGame
+        return combined_tensor
 
     def step(self, action):
         if action == 0:
             self.body_element.send_keys(Keys.ARROW_UP)  # Saltar
         elif action == 1:
             self.body_element.send_keys(Keys.ARROW_DOWN)  # Agacharse
+        
+        #No defino la acción 3 que es no hacer nada porque no hace nada xd
+            
 
-        combined_tensor, finPartida= self.get_game_state()
+        combined_tensor= self.get_game_state()
 
         done = False
+        reward = 0 
 
-        time.sleep(self.SLEEP_TIME)
-
-        if finPartida:
-            self.counter += 1
-        else:
-            self.counter = 0
-
-        #if self.counter * self.SLEEP_TIME >= self.TIME_THRESHOLD:
-        if self.counter >2:
+        if self.hasDied():
             logging.info("Encontrado final")
             self.body_element.send_keys(Keys.ARROW_UP)
-            self.counter = 0
             done = True
-        
-
-        # Actualizar la observación
-        #combined_tensor, finPartida = self.get_game_state()
-
-        reward = 1  # Puede personalizar la función de recompensa según tus necesidades
-        self.points += 1
+            print("Puntos antes de muerte: "+str(self.points))
+            reward=-(self.DIED_CONSTANT/self.points)
+            self.points += reward
+        else:
+            reward = 1 
+            self.points += 1
 
         return combined_tensor, reward, done, False,  {}
     
@@ -148,10 +131,13 @@ class DinoEnv(gym.Env):
         time.sleep(3)
         self.counter = 0
         self.points = 0
-        combined_tensor, _ = self.get_game_state()  # Descomposición de la tupla para obtener solo la observación
+        combined_tensor= self.get_game_state()  # Descomposición de la tupla para obtener solo la observación
         return combined_tensor.astype(np.float32), {}
 
 
+    def hasDied(self):
+        dino=self.driver.execute_script("return Runner.instance_.crashed;")
+        return dino
 
     def render(self, mode='human'):
         pass
